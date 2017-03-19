8baf9d0895691f3576d02ecd56efadc52636aa5b
Sascha <limper545@live.com>
2017-03-19 17:29:52 +0100

    Header auf eigene anforderungen angepasst

--- view/aromeda/Components/Header/header.component.js
+++ view/aromeda/Components/Header/header.component.js
@@ -2,15 +2,9 @@ angular.module('header', [])
     .component('header', {
         templateUrl: 'Components/Header/header.html',
         controller: function ($scope, $location, toastr, socket, $interval) {
-            $scope.cookie2 = undefined;
-            if ($scope.cookie2 == undefined) {
-                $interval(function () {
-                    $scope.cookie2 = $.cookie("session");
-                }, 200)
+
+            $scope.auswahlHead = function (headData) {
+                $scope.auswahl = headData;
             }
-            // $scope.test = function () {
-            //     $.removeCookie("session");
-            //     $scope.cookie = '';
-            // };
         }
     });
