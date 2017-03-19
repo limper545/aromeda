e89330e72d01a154ede5bbb056fd0c9d4035d656
Sascha <limper545@live.com>
2017-03-19 17:46:53 +0100

    Header anzeige abhängig vom Status

--- view/aromeda/Components/Header/header.component.js
+++ view/aromeda/Components/Header/header.component.js
@@ -2,9 +2,19 @@ angular.module('header', [])
     .component('header', {
         templateUrl: 'Components/Header/header.html',
         controller: function ($scope, $location, toastr, socket, $interval) {
-
             $scope.auswahlHead = function (headData) {
                 $scope.auswahl = headData;
-            }
+            };
+
+            stop = $interval(function () {
+                console.log('Interval');
+                if ($.cookie("session")) {
+
+                    $scope.showHeader = true;
+                    $interval.cancel(stop);
+                } else {
+                    $scope.showHeader = false;
+                }
+            }, 500)
         }
     });
